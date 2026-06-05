98f73e539add06ccba575c9e1a68711854c00627
Daniels_08 <jansa.daniel08@gmail.com>
2026-06-05 12:00:26 +0100

    Add files via upload

---
+++ Konfigurační_šablona_proměnných_prostředí.bash
@@ -0,0 +1,11 @@
+# Šablona konfiguračních proměnných prostředí pro web Dominik Anděl
+# Zkopírujte tento soubor jako .env v produkčním Netlify rozhraní a doplňte reálné hodnoty.
+
+# ID měření Google Analytics (G-XXXXXXXXXX)
+GOOGLE_ANALYTICS_ID=G-PLACEHOLDER
+
+# E-mail, na který mají chodit poptávky z kontaktního formuláře (výchozí je Dominikův e-mail)
+CONTACT_FORM_RECIPIENT=dominik.andel.beroun@example.com
+
+# Formspree nebo Netlify Forms Endpoint (pokud se přejde z lokálního JS odesílání na externí službu)
+FORM_API_ENDPOINT=https://formspree.io/f/placeholder
